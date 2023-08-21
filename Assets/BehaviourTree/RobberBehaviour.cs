31879e4b2ed03904058af262cae12689ef4f6f49
Arshia RM <82693306+ArshiaRx@users.noreply.github.com>
2023-08-21 19:53:39 -0400

    Update RobberBehaviour.cs

--- Assets/BehaviourTree/RobberBehaviour.cs
+++ Assets/BehaviourTree/RobberBehaviour.cs
@@ -8,41 +8,76 @@ public class RobberBehaviour : MonoBehaviour
     BehaviourTree tree;
     public GameObject diamond;
     public GameObject van;
+    public GameObject backdoor;
+
     NavMeshAgent agent;
 
+    public enum ActionState { IDLE, WORKING };
+    ActionState state = ActionState.IDLE;
+
+    Node.Status treeStatus = Node.Status.RUNNING;
+
+    // Start is called before the first frame update
     void Start()
     {
         agent = this.GetComponent<NavMeshAgent>();
 
         tree = new BehaviourTree();
-        Node steal = new Node("Steal Something");
+        Sequence steal = new Sequence("Steal Something");
         Leaf goToDiamond = new Leaf("Go To Diamond", GoToDiamond);
+        Leaf goToBackDoor = new Leaf("Go To Back Door", GoToBackDoor);
         Leaf goToVan = new Leaf("Go To Van", GoToVan);
 
+        //steal.AddChild(goToBackDoor);
         steal.AddChild(goToDiamond);
+        steal.AddChild(goToBackDoor);
         steal.AddChild(goToVan);
+
         tree.AddChild(steal);
 
         tree.PrintTree(); 
 
-        tree.Process();
     }
 
     public Node.Status GoToDiamond()
     {
-        agent.SetDestination(diamond.transform.position);
-        return Node.Status.SUCCESS;
+       return GoToLocation(diamond.transform.position);
+    }
+
+    public Node.Status GoToBackDoor()
+    {
+        return GoToLocation(backdoor.transform.position);
     }
 
     public Node.Status GoToVan()
     {
-        agent.SetDestination(van.transform.position);
-        return Node.Status.SUCCESS;
+        return GoToLocation(van.transform.position);
+    }
+
+    Node.Status GoToLocation(Vector3 destination)
+    {
+        float distanceToTarget = Vector3.Distance(destination, this.transform.position);
+        if (state == ActionState.IDLE)
+        {
+            agent.SetDestination(destination);
+            state = ActionState.WORKING;
+        }
+        else if (Vector3.Distance(agent.pathEndPosition, destination) >=2 )
+        {
+            state = ActionState.IDLE;
+            return Node.Status.FAILURE;
+        }
+        else if (distanceToTarget < 2)
+        {
+            state = ActionState.IDLE;
+            return Node.Status.SUCCESS;
+        }
+        return Node.Status.RUNNING;
     }
 
     void Update()
     {
-
-    }
-    
+        if (treeStatus == Node.Status.RUNNING)
+            treeStatus = tree.Process();
+    }   
 }
