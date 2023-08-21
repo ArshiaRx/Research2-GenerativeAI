b2bd50a065cd616ee877347e39a5b299e5fd72f3
Arshia RM <82693306+ArshiaRx@users.noreply.github.com>
2023-08-21 16:52:47 -0400

    Update RobberBehaviour.cs

--- Assets/BehaviourTree/RobberBehaviour.cs
+++ Assets/BehaviourTree/RobberBehaviour.cs
@@ -1,23 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class RobberBehaviour : MonoBehaviour
 {
     BehaviourTree tree;
+    public GameObject diamond;
+    public GameObject van;
+    NavMeshAgent agent;
 
     void Start()
     {
+        agent = this.GetComponent<NavMeshAgent>();
+
         tree = new BehaviourTree();
         Node steal = new Node("Steal Something");
-        Node goToDiamond = new Node("Go To Diamond");
-        Node goToVan = new Node("Go To Van");
+        Leaf goToDiamond = new Leaf("Go To Diamond", GoToDiamond);
+        Leaf goToVan = new Leaf("Go To Van", GoToVan);
 
         steal.AddChild(goToDiamond);
         steal.AddChild(goToVan);
         tree.AddChild(steal);
 
         tree.PrintTree(); 
+
+        tree.Process();
+    }
+
+    public Node.Status GoToDiamond()
+    {
+        agent.SetDestination(diamond.transform.position);
+        return Node.Status.SUCCESS;
+    }
+
+    public Node.Status GoToVan()
+    {
+        agent.SetDestination(van.transform.position);
+        return Node.Status.SUCCESS;
     }
 
     void Update()
