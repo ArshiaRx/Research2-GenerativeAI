9c3f39811f3d1466f61dd0a9b0e8bf670946c2f7
Arshia RM <82693306+ArshiaRx@users.noreply.github.com>
2024-09-04 15:12:02 -0400

    Update BehaviourTree.cs

--- Assets/BehaviourTree/BehaviourTree.cs
+++ Assets/BehaviourTree/BehaviourTree.cs
@@ -2,13 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BehaviourTree : Node
+public class BehaviourTree : Node             // Behaviour Tree inherits from Node
 {
+
+    // Behaviour Tree Constructor
     public BehaviourTree(){
-        name = "Tree";
+        name = "Tree";                        // Name of this class "Tree"
     }
 
-    public BehaviourTree(string n){
+    // Constructor with one parameter
+    public BehaviourTree(string n){            // Any name the BehaviourTree Node has
         name = n;
     }
 
@@ -23,22 +26,25 @@ public class BehaviourTree : Node
         public Node node;
     }
 
+    // Method to print the Tree Nodes
     public void PrintTree()
     {
-        string treePrintout = "";
-        Stack<NodeLevel> nodeStack = new Stack<NodeLevel>();
-        Node currentNode = this;
-        nodeStack.Push(new NodeLevel { Level = 0, node = currentNode } );
+        // Can solve this using recursion or without recursion. This solution is without recursion
+        
+        string treePrintout = "";                                   // emptry string
+        Stack<NodeLevel> nodeStack = new Stack<NodeLevel>();           // new stack of Nodes
+        Node currentNode = this;                                        // This is the currentNode
+        nodeStack.Push(new NodeLevel { Level = 0, node = currentNode } );         // Push - Add this currentNode to the stack
 
-        while (nodeStack.Count != 0){
+        while (nodeStack.Count != 0){                                // while stack not empty do the following
 
-            NodeLevel nextNode = nodeStack.Pop();
+            NodeLevel nextNode = nodeStack.Pop();                    // nextNode will remove one Node from the stack top
             treePrintout += new string ('-', nextNode.Level) + nextNode.node.name + "\n";
-            for (int i = nextNode.node.children.Count - 1; i >= 0; i--)
+            for (int i = nextNode.node.children.Count - 1; i >= 0; i--)          // Decrement from the bottom node (stack top)
             {
                 nodeStack.Push( new NodeLevel { Level = nextNode.Level + 1, node = nextNode.node.children[i] });
             }
         }
-        Debug.Log(treePrintout);
+        Debug.Log(treePrintout);    // output to the console
     }
 }
